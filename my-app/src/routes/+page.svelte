cee38411233c65926ae26098d5311ece9599eee5
John Does it <hello@johndoesit.be>
2025-04-16 08:19:04 -0700

    remove font awesome cdn

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  /* root - +page.svelte */
   let firstName: string = 'John'
   let lastName: string = 'Doe'
   let role: string = 'APM Manager'
@@ -10,7 +11,6 @@
 <svelte:head>
   <title>✍️ Générateur de signature d'email personnalisée pour RESA</title>
   <meta name="description" content="Générer votre signature d'email RESA personnalisée en toute simplicité !">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </svelte:head>
 
 <wrapper>
