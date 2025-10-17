3e2495c75c0fd6c7e6d2b81dc1d8852fb801cdfc
John Does it <hello@johndoesit.be>
2025-10-17 13:32:57 +0200

    Remove unused social media logo images
    Deleted logo images for Facebook, LinkedIn, Resa, and YouTube from the static directory. Also made a minor formatting update in +layout.svelte. This cleans up unused assets and improves code clarity.

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -4,6 +4,7 @@
   
   import { dev } from '$app/environment'
 	import { injectAnalytics } from '@vercel/analytics/sveltekit'
+  
 	injectAnalytics({ mode: dev ? 'development' : 'production' })
 </script>
 
